74ee2fe658a2377ebb0fabc70f6f688f87150137
TimMarder <tmarder@stuy.edu>
2019-01-11 10:25:20 -0500

    board stuff'

---
+++ boardgen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+
+    printf("\n");
+
+    printf("    0 1 2 3 4 5 6 7 8 9\n");
+    printf("  -----------------------\n");
+    printf("A | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("B | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("C | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("D | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("E | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("F | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("G | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("H | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("I | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("J | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |\n");
+    printf("  -----------------------\n");
+
+}
